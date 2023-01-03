b1c5c26cd786f1a549d4fb96c1544f7a16901779
Marc Lamy <marclelamym@gmail.com>
2023-01-02 20:30:04 -0500

    adding summary gen and manual labelling .pys

--- 3-1_generate_summary.py
+++ 3-1_generate_summary.py
@@ -1,4 +1,5 @@
 import src.useful as su
+import src.labelling as sl
 from PIL import Image, ImageDraw, ImageFont, UnidentifiedImageError
 from caffeine import on
 
@@ -11,10 +12,8 @@ import plotly.io as pio
 import os
 import shutil
 
-from tqdm.contrib.concurrent import process_map 
-
-pd.set_option("display.max_columns", None)
-pio.templates.default = "plotly_dark"
+from tqdm.contrib.concurrent import process_map
+from tqdm import tqdm
 
 # Creating SQL database to store all the data for the project
 database = "data/main_database.sqlite"
@@ -23,108 +22,59 @@ con = sqlite3.connect(database)
 
 
 
-# Load the data and display only the one from a 
-query = '''
-select 
-    * 
-from clean_comments_users_last12
-'''
-df_main = pd.read_sql_query(query, con)
+def load_users_to_label():
+    # List usernames to be proposed
+    query = '''
+    with all_labels as (
+    select * from labels 
+    union all 
+    select * from manual_labelling
+    )
 
-def generate_card(username):
-    df_user = df_main[df_main["username"]==username].reset_index(drop=True)
-
-    # Create new image
-    global_image = Image.new("RGB", (1300, 450), "white")
-    draw = ImageDraw.Draw(global_image)
-    font = ImageFont.truetype("OpenSans-Light.ttf", 15)
-
-    # Open user pp and add it to the global image
-    try:
-        path = f"data/photos/user_profile_pictures/{username}_pp_user_photo.png"
-        profile_pic = Image.open(path)
-        global_image.paste(profile_pic, (250, 0))
-    except (FileNotFoundError, UnidentifiedImageError): 
-        pass
-
-    # Creating a list of position to add the photos on the global image
-    positions = []
-    for x in range(850, 1300, 150):
-        for y in range(0, 450, 150):
-            positions.append((x, y))
+    select 
+    distinct 
+        ccul.username 
+        , l.label
+        , l.label2
+        , round(p.prediction, 5) prediction
 
 
-    # Loop trhough each image and add it to the global image
-    # user_last12_path = f"data/7_user_last_12_csv/{username}_last12.csv"
-    # if os.path.exists(user_last12_path):
-    #     last_12 = pd.read_csv(user_last12_path)
-    for image_num in range(12):
-        path = f"data/photos/user_last_12_posts/{username}_{str(image_num)}_user_photo.png"
-        
-        try:
-            if os.path.exists(path):
-                img = Image.open(path)
-                if image_num < 9:
-                    global_image.paste(img.resize((150, 150)), positions[image_num])
-                elif image_num == 9:
-                    global_image.paste(img.resize((150, 150)), (400, 0))
-                elif image_num == 10:   
-                    global_image.paste(img.resize((150, 150)), (550, 0))
-                elif image_num == 11:
-                    global_image.paste(img.resize((150, 150)), (700, 0))
-        except (OSError, UnidentifiedImageError) as e: # For truncated image file
-            print("OSError/PIL: ", e)
+    from clean_comments_users_last12 ccul
+    left join all_labels l using(username)
+    left join predictions p using(username)
+    where 1=1
+        and l.label2 is null
+        and (l.labelling_technique is null or l.labelling_technique = 'manual_labelling')
+    order by l.label desc, p.prediction desc'''
+
+    return pd.read_sql_query(query, con)
 
 
 
-    # Add basic information 
-    username = df_user.loc[0, "username"]
-    draw.text((10, 5), f"Username: {username}", font=font, fill="black")
+def load_users_to_label():
+    # List usernames to be proposed
+    query = '''
+            with all_labels as (
+            select distinct username, label, labelling_technique from labels
+            union all 
+            select distinct username, label, labelling_technique from manual_labelling
+            )
+            select 
+                p.username
+                , count(username) as count
+                , group_concat(labelling_technique) as labelling_technique
+                , p.entropy
+            from predictions p
+            left join all_labels a using (username)
+            where entropy > 0.2 or labelling_technique is null
+            group by 1
+            having group_concat(labelling_technique) = 'manual_labelling' or a.labelling_technique is null
+            order by entropy desc
+            '''
 
-    follower_count = df_user.loc[0, "follower_count"]
-    color = "black" if follower_count > 800 else "red"
-    draw.text((10, 25), f"Followers: {follower_count:,}", font=font, fill=color)
-
-    follow_count = df_user.loc[0, "follow_count"]
-    draw.text((10, 45), f"Following: {follow_count:,}", font=font, fill="black")
-
-    post_count = df_user.loc[0, "post_count"]
-    color = "black" if post_count > 16 else "red"
-    draw.text((10, 65), f"Post count: {post_count}", font=font, fill=color)
-
-    video_count = df_user.loc[0, "video_count"]
-    color = "black" if video_count > 0 else "red"
-    draw.text((10, 85), f"Video count: {video_count}", font=font, fill=color)
-
-    comment_likes = round(df_user["comment_likes"].mean())
-    color = "black" if 200 <= comment_likes < 450 else "red"
-    draw.text((10, 105), f"Comment_likes: {comment_likes}", font=font, fill="black")
-
-    domain = df_user.loc[0, "domain"]
-    draw.text((10, 125), f"{domain}", font=font, fill="black")
-
-    biography = df_user.loc[0, "biography"]
-    draw.text((10, 145), f"{biography}", font=font, fill="black")
-
-    comment = '\n'.join(df_user["comment"])
-    draw.text((10, 145), f"{comment}", font=font, fill="black")
-
-
-    # Save screenshot, open it, resize it, add it to global_image and deleting the file
-    path = f"data/photos/bio_url_screenshot/{username}_website_photo.png"
-    try:
-        screenshot = Image.open(path)
-        global_image.paste(screenshot.resize((500, 300)), (350, 150))
-    except (UnidentifiedImageError, FileNotFoundError):
-        pass
-
-
-    # Export image
-    summary_path = f"data/photos/image_summary/{username}_image_summary.png"
-    global_image.save(summary_path)
-    pd.DataFrame({'username': [username], 
-                  'photo_type': ['summary'], 
-                  'path': [path]}).to_sql('photos', con, if_exists='append', index=False)
+    labelled_usernames = sl.load_labels()['username'].tolist()
+    df = pd.read_sql_query(query, con).query('username not in @labelled_usernames').reset_index(drop=True)
+    return df
 
 
 
@@ -143,13 +93,39 @@ if __name__ == '__main__':
         distinct
         username
     from clean_comments_users_last12
-    where username not in (select username from photos where photo_type = 'summary')
+    --where username not in (select username from photos where photo_type = 'summary')
     '''
-    usernames = pd.read_sql_query(query, con)
+
+    # query = '''
+    # select 
+    #     distinct 
+    #     username 
+    # from force_summary_generation
+    # '''
+
+    # query = '''
+    # select 
+    # distinct 
+    #     ccul.username 
+
+    # from clean_comments_users_last12 ccul
+    # left join labels l using(username)
+    # left join predictions p using(username)
+    # where 1=1
+    #     and l.label2 is null
+    #     and (l.labelling_technique is null or l.labelling_technique = 'manual_labelling')
+    # order by l.label desc, p.prediction desc'''
+
+    done_usernames = sl.list_files('data/photos/image_summary', common=False)
+    usernames = load_users_to_label()['username'].unique()
+    print(len(usernames))
+    # usernames = [username for username in tqdm(usernames) if username not in done_usernames]
+    print(len(usernames))
 
 
 
-    print("\n", len(usernames["username"]), usernames.loc[0, "username"])
+    # print("\n", len(usernames["username"]), usernames.loc[0, "username"])
+    chunksize = len(usernames) // (8 * 4)
+    process_map(sl.generate_card, usernames, max_workers=8, chunksize=chunksize)
 
-    process_map(generate_card, usernames["username"], max_workers=8, chunksize=1)
 
--- 3-2_manual_labelling.py
+++ 3-2_manual_labelling.py
@@ -1,5 +1,7 @@
+import src.labelling as sl
+
 import pandas as pd 
-import sqlite3 
+import sqlite3  
 
 import pygame 
 
@@ -32,23 +34,68 @@ pygame.display.set_caption('Image')
 def load_users_to_label():
     # List usernames to be proposed
     query = '''
+            with all_labels as (
+            select distinct * from labels
+            union all 
+            select distinct * from manual_labelling
+            )
+            select 
+                p.username
+                , p.legit_proba	
+                , p.bot_proba
+                , p.predicted_label
+                , p.entropy
+            from predictions p
+            left join all_labels a using (username)
+            where entropy > 0.2 or labelling_technique is null
+            group by 1
+            having group_concat(labelling_technique) = 'manual_labelling' or a.labelling_technique is null
+            order by entropy desc
+            '''
+    query = f'''
+    with all_labels as (
     select 
         distinct 
         username 
-    from clean_comments_users_last12
-    left join labels using(username)
-    where 1=1
-        and label2 is null 
-    order by label desc'''
+        , group_concat(labelling_technique)  as labelling_technique
+        
+    from (select distinct * from labels
+            union all 
+            select distinct * from manual_labelling) 
+    group by 1
+    )
 
-    users_to_label = pd.read_sql_query(query, con)['username']
-    return list(users_to_label)
+    select 
+        p.username
+        , p.legit_proba	
+        , p.bot_proba
+        , p.predicted_label
+        , p.entropy
+    from predictions p
+    left join all_labels a using(username)
+    where labelling_technique = 'manual_labelling' or a.username is null
+    group by 1,2,3,4,5
+    order by p.predicted_label desc, entropy desc
+    ''' 
+    
+
+    # showing users with russian characters first
+    # chars = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Х', 'Ч', 'Ш', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
+    # usernames_russe = sl.load_main().query('comment.str.contains("|".join(@chars)) or biography.str.contains("|".join(@chars))')['username'].unique()
+
+    # users with high porno or porno + sexy score (bots are mainly porn, )
+    usernames_sexy = pd.read_sql_query('select username from nsfw group by 1 having avg(porn + sexy) > .5', con)['username'].unique()
+
+    df = pd.read_sql_query(query, con)
+    # df = df.query('username in @usernames_russe').reset_index(drop=True)
+    df = df.query('username in @usernames_sexy').reset_index(drop=True)
+    return df
 
 
 
 def subset_user_data(username):
     # Create a subset of main 
-    df_user = df_main[df_main["username"]==username].reset_index(drop=True)
+    df_user = df_main[df_main['username']==username].reset_index(drop=True)
     return df_user
 
 
@@ -59,7 +106,7 @@ def load_labelled_users():
     select 
         distinct 
         * 
-    from labels'''
+    from manual_labelling'''
 
     users_to_label = pd.read_sql_query(query, con)
     return users_to_label
@@ -77,10 +124,11 @@ def update_image(image_user_path):
 def which_key():
     '''Return which arrow key pressed'''
 
-    key_direction = {pygame.K_LEFT: "left",
-                     pygame.K_RIGHT: "right",
-                     pygame.K_DOWN: "down",
-                     pygame.K_UP: "up"}
+    key_direction = {pygame.K_LEFT: 'left',
+                     pygame.K_RIGHT: 'right',
+                     pygame.K_DOWN: 'down',
+                     pygame.K_UP: 'up',
+                     pygame.K_o: 'o'}
 
     while True:
         events = pygame.event.get()
@@ -96,22 +144,20 @@ def which_key():
 
 
 
-def label_username(df_all_labels, username, label, first_label):
+def label_user(username, label):
     # Add result to a dataframe and export it
-    result = [username, label, datetime.now(), "manual_labelling"]
-    print(df_all_labels)
-    if username not in df_all_labels['username'].tolist():
-        df_all_labels.loc[len(df_all_labels), ['username', 'label', 'time', 'labelling_technique']] = result
-    else:
-        df_all_labels.loc[df_all_labels['username']==username, ['username', 'label2', 'time2', 'labelling_technique']] = result
-    print(df_all_labels)
-
-    return df_all_labels
+    # conn = sqlite3.connect('data/main_database.sqlite')
+    dt = datetime.now()
+    result = [username, label, dt, 'manual_labelling']
+    df = pd.DataFrame([result], columns=['username', 'label', 'time', 'labelling_technique'])
+    print(df)
+    df.to_sql('manual_labelling', con, if_exists='append', index=False)
+    return dt
 
 
 
 
-def terminal_infos(df_current_user):
+def terminal_infos(df_current_user, color):
     '''Infos to print in the terminal for each user
     '''
     str_to_print = ''
@@ -119,31 +165,23 @@ def terminal_infos(df_current_user):
     for comment in df_current_user['comment']:
         str_to_print += comment + '\n'
     
-    emoji_dict = df_current_user["emoji_dict"].values[0]
+    emoji_dict = df_current_user['emoji_dict'].values[0]
     for key, value in eval(emoji_dict).items():
-        str_to_print += f"{key} {value} | "
+        str_to_print += f'{key} {value} | '
+
+    str_to_print += '\n' * 5 + color
 
     print(str_to_print)
 
 
 
-def modify_label(username, label):
-    '''Modify existing label'''
-    df_all_labels = load_labelled_users()
-    
-    if pd.isna(df_all_labels.loc[df_all_labels['username'] == username, 'label2']): 
-        col = ''
-    else:
-        col = '2'
-
+def update_label(label, dt):
     query = f'''
-            update labels
-            set
-                label{col} = {label}
-            where username == {username}
-            '''
+    UPDATE manual_labelling
+    SET label = {label}
+    WHERE time = "{dt}"'''
 
-    con.execute(query)    
+    con.execute(query)
 
 
 
@@ -175,43 +213,128 @@ def train_model():
             pass
 
 
+def propose_user(username):
+    df_current_user = subset_user_data(username)
+
+    update_image(f'data/photos/image_summary/{username}_image_summary.png')
+
+
+
+def prediction_color (next_prediction):
+    if next_prediction >= .50: 
+        return '🟥'*3000
+    elif .50 > next_prediction >= .30: 
+        return '🟧'*3000
+    else:
+        return '🟩'*3000
+
+
+
 def main():
-    users_to_label = load_users_to_label()
-    df_all_labels = load_labelled_users()
+    df_users_to_label = load_users_to_label()
+    print(f'{df_users_to_label.shape[0] = }')
 
-    for index, username in enumerate(users_to_label):
-        image_user_path = f"data/photos/image_summary/{username}_image_summary.png"
-        if os.path.exists(image_user_path) == False:
-            continue
-        else:
-            update_image(image_user_path)
-
+    label_count = [0, 0]
+    time_start = time.time()
+    for index in range(df_users_to_label.shape[0]): 
+        print(df_users_to_label.iloc[index-10:index+20].head(30))
+        # If labelled 500 or spent 10 minutes, stop the program
+        time_diff = int(time.time() - time_start)
+        if sum(label_count) == 500 or time_diff > 600: 
+            break
 
         # Loading user's data and checking if already labelled
-        df_current_user = subset_user_data(username)
-        df_last_user = subset_user_data(users_to_label[index-1])
-        df_next_user = subset_user_data(users_to_label[index+1])
-        first_label = username not in df_all_labels['username']
+        current_username, current_legit_proba, current_bot_proba, current_prediction, current_entropy = df_users_to_label.loc[index, :]
+        df_current_user = subset_user_data(current_username)
+        next_index = index
+        if index < df_users_to_label.shape[0] - 2: 
+            next_index += 1
+        next_username, next_prediction, next_entropy = df_users_to_label.loc[next_index,['username', 'predicted_label', 'entropy']]
+        df_next_user = subset_user_data(next_username)
+            
+        if index > 0: 
+            past_username, past_prediction, past_entropy = df_users_to_label.loc[index-1,['username', 'predicted_label', 'entropy']]
+            df_past_user = subset_user_data(past_username)
 
-        if index % 5 == 0: 
-            train_model()
-        terminal_infos(df_current_user)
+
+        # Print past rows labelled
+        print(pd.read_sql_query('select * from manual_labelling', con).tail(5), '\n'*5)
+
+        # Generate summary if the user doesn't have one
+        image_user_path = f'data/photos/image_summary/{current_username}_image_summary.png'
+        if os.path.exists(image_user_path) == False:
+            # sl.generate_card(current_username)
+            continue
+
+ 
+        # Propose a user and print to the 
+        try: 
+            propose_user(current_username)
+        except pygame.error:
+            print(f'pygame.error: Error reading the PNG file for user', current_username)
+            continue
+        
+        # Print major infos to terminal
+        color = prediction_color(next_prediction)
+        terminal_infos(df_current_user, color)
+        print(sl.load_table('manual_labelling').query('username == @current_username'), '\n'*5)
+        print(f'{current_username = }\n{current_prediction = }\n{next_prediction = }\n{current_entropy = }\n{current_legit_proba = }\n{current_bot_proba = }\n{label_count = }\n{sum(label_count) = }\n{time_diff = }')
+        print(f'{df_users_to_label.shape[0] - index = }')
 
         # Capture which key pressed and export
         key_pressed = which_key()
         
-        key_press_choices = {"right": 'bot', "up": 'legit', "down": 'maybe'}
+        key_press_choices = {'right': 1, 'up': 0, 'down': 2, 'o': 3}
         if key_pressed == 'left':
+            propose_user(past_username)
             key_pressed = which_key()
-            modify_label(username, key_press_choices[key_pressed]) 
+            update_label(key_press_choices[key_pressed], past_label_dt)
+
         
-        elif key_pressed in ('right', 'up', 'down'):
-            df_all_labels = label_username(df_all_labels, username, key_press_choices[key_pressed], first_label)
-            
-        df_all_labels.to_sql('labels', con, if_exists='replace', index=False)
+        elif key_pressed in key_press_choices.keys():
+            past_label_dt = label_user(current_username, key_press_choices[key_pressed])
+            if key_pressed == 'right': 
+                label_count[0] += 1
+            elif key_pressed == 'up': 
+                label_count[0] += 1
 
-        print(df_all_labels, '\n'*5)
 
+        # df_all_labels = pd.concat([df_all_labels, df_user_labelled], axis=0)
+
+        # print(df_all_labels, '\n'*5)
+
+
+# def which_key():
+#     '''Return which arrow key pressed'''
+
+#     key_direction = {pygame.K_LEFT: 'left',
+#                      pygame.K_RIGHT: 'right',
+#                      pygame.K_DOWN: 'down',
+#                      pygame.K_UP: 'up'}
+
+#     while True:
+#         events = pygame.event.get()
+#         for event in events:
+#             if event.type == pygame.KEYDOWN:
+#                 if event.key in key_direction.keys():
+#                     key_direction[event.key]
+#                     break
+#                 else:
+#                     print('WRONG KEY, PRESS AGAIN')
+
+
+#     key_press_choices = {'right': 1, 'up': 0, 'down': 2}
+#     if key_pressed == 'left':
+#         # key_pressed = which_key()
+#         # modify_label(username, key_press_choices[key_pressed]) 
+#         propose_user(username)
+    
+#     elif key_pressed in key_press_choices.keys():
+#         df_all_labels = label_username(df_all_labels, username, key_press_choices[key_pressed])
+#         if key_pressed == 'right': 
+#             count[0] += 1
+#         if key_pressed == 'up': 
+#             count[0] += 1
 
 
 main()
